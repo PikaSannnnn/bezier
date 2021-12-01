b6dd0a2c3140b149e16b3bd817531d01482eb5b6
Pikasannnnn <dong.stephen11@gmail.com>
2021-12-01 15:43:34 -0800

    Mostly done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-// Name:
-// Quarter, Year:
+// Name: Stephen Dong
+// Quarter, Year: Fall 2021
 // Lab:
 //
 // This file is to be modified by the student.
@@ -21,7 +21,43 @@ using namespace std;
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
-vector<vec2> points;
+vector<vec2> points(0);
+float* c;
+
+// float Factorial(int n) {
+//     if (n == 0) return 1;
+//     return n * Factorial(n-1);
+// }
+
+// float Combination(int n, int k) {
+//     return Factorial(n) / (Factorial(k) * Factorial(n - k));
+// }
+
+// float Binomial(int n, int k, float t) { // n = pointsize, k = curpoint, t = part of line
+//     return Combination(n, k) * pow(t, k) * pow(1- t, n - k);
+// }
+
+void coefficients(float* c, int n, float t) {   // n = size, t = part of line
+    float* r = new float[n + 1];
+    float* s = new float[n + 1];
+    r[0] = 1;
+    s[n] = 1;
+
+    for (int i = 1; i < n + 1; i++) {
+        r[i] = (t * ((float)(n - i + 1) / i)) * r[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        s[i] = (1 - t) * s[i + 1];
+    }
+
+    for (int i = 0; i < n + 1; i++) {
+        c[i] = r[i] * s[i];
+    }
+
+    delete[] r;
+    delete[] s;
+}
 
 void GL_render()
 {
@@ -29,13 +65,30 @@ void GL_render()
     glutSwapBuffers();
 
     glBegin(GL_LINES);
-    glColor3f(1.0f,0.0f,0.0f);
+    glColor3f(0.50f,0.45f,1.0f);
 
     // TODO: just for example, you will need to change this.
-    glVertex2f(-.5f,-.5f);
-    glVertex2f(.5f,-.5f);
-    glVertex2f(.5f,.5f);
-    glVertex2f(-.5f,.5f);
+    for (unsigned i = 1; i < points.size(); i++) {  // control points
+        glVertex2f(points[i][0], points[i][1]);
+        glVertex2f(points[i - 1][0], points[i - 1][1]);
+    }
+
+    for (float t = 0; t <= 1.001; t += 0.01) {   // bezier curve
+        float* c = new float[points.size() + 1];
+        coefficients(c, points.size(), t);
+
+        vec2 sum(0.0,0.0);
+        for (unsigned i = 0; i < points.size(); i++) {
+            sum[0] += c[i] * points[i][0];
+            sum[1] += c[i] * points[i][1];
+        }
+    
+        glVertex2f(sum[0], sum[1]);
+
+        delete[] c;
+    }
+
+    // if(points.size()) cout << points[points.size() - 1][0] << ',' << points[points.size() - 1][1] << endl;
 
     glEnd();
     glFlush();
@@ -55,6 +108,9 @@ void GL_mouse(int button,int state,int x,int y)
         double px,py,dummy_z; // we don't care about the z-value but need something to pass in
         gluUnProject(x,y,0,mv_mat,proj_mat,vp_mat,&px,&py,&dummy_z);
         // TODO: the mouse click coordinates are (px,py).
+        vec2 pt(px, py);
+        points.push_back(pt);
+
         glutPostRedisplay();
     }
 }
@@ -68,7 +124,7 @@ void GLInit(int* argc, char** argv)
 
     //glMatrixMode(GL_PROJECTION_MATRIX);
     //glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, -1, 1);
-    glutCreateWindow("CS 130 - <Insert Name Here>");
+    glutCreateWindow("CS 130 - <Stephen Dong>");
     glutDisplayFunc(GL_render);
     glutMouseFunc(GL_mouse);
 }
